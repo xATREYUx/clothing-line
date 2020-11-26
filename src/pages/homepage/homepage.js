170122e50b7993d3a501b2abfc78f1f4c410763c
xATREYUx <mwmailing@gmail.com>
2020-11-26 01:26:47 -0500

    added menu items style

---
+++ src/pages/homepage/homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Directory from "../../components/directory/directory";
+import "./homepage.scss";
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default HomePage;
