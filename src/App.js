5be92d8388da6c7d685a880af83c7d45174844ef
xATREYUx <mwmailing@gmail.com>
2020-11-26 22:06:46 -0500

    update menu-items with routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage";
+import { Route, Switch } from "react-router-dom";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
